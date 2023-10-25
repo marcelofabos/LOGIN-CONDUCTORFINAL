9945bbf5642a8009b6a9185efe059bd91cda6533
marcelo <marcelooscco1@gmail.com>
2023-10-25 12:21:29 -0500

    Agregar archivos de proyecto.

---
+++ Login-Conductor.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LOGIN_CONDUCTORFINAL
+{
+    public partial class Login_Conductor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            string conectar = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            SqlConnection sqlConectar = new SqlConnection(conectar);
+            SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", sqlConectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Connection.Open();
+            cmd.Parameters.Add("@Usuario", SqlDbType.VarChar, 50).Value = tbUsuario.Text;
+            cmd.Parameters.Add("@Password", SqlDbType.VarChar, 100).Value = tbPassword.Text;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                //Agregamos una sesion de usuario
+                Session["usuariologueado"] = tbUsuario.Text;
+                Response.Redirect("Index.aspx");
+            }
+            else
+            {
+                lblError.Text = "Error de Usuario o Contrasenia";
+            }
+
+            cmd.Connection.Close();
+        }
+
+
+        
+    }
+}
